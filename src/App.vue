5ab59b25ad9490e792aabf10623cbf2ad47ddeea
ferreira-tb <109777783+ferreira-tb@users.noreply.github.com>
2023-02-22 20:59:32 -0300

    Commit inicial

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+    <RouterView v-slot="{ Component }">
+        <template v-if="Component">
+            <Transition name="fade" mode="out-in">
+                <Suspense>
+                    <component :is="Component" />
+                    <template #fallback>
+                        <span class="loading-text">Carregando...</span>
+                    </template>
+                </Suspense>
+            </Transition>
+        </template>
+    </RouterView>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <main>
+        No princípio, a Terra era sem forma e vazia.
+    </main>
+</template>
+
+<style scoped>
+</style>
