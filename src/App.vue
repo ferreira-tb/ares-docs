78517af56723275a4627c86db447bbc20a491c9e
ferreira-tb <109777783+ferreira-tb@users.noreply.github.com>
2023-03-09 23:42:43 -0300

    chore: adiciona ícones e atualiza estilos

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import { NConfigProvider, darkTheme } from 'naive-ui';
+import NavBar from '@/components/NavBar.vue';
 </script>
 
 <template>
-    <RouterView v-slot="{ Component }">
-        <template v-if="Component">
-            <Transition name="fade" mode="out-in">
-                <Suspense>
-                    <component :is="Component" />
-                    <template #fallback>
-                        <span class="loading-text">Carregando...</span>
-                    </template>
-                </Suspense>
-            </Transition>
-        </template>
-    </RouterView>
+    <NConfigProvider :theme="darkTheme">
+        <NavBar />
+        <RouterView v-slot="{ Component }">
+            <template v-if="Component">
+                <Transition name="fade" mode="out-in">
+                    <Suspense>
+                        <component :is="Component" />
+                        <template #fallback>
+                            <span class="loading-text">Carregando...</span>
+                        </template>
+                    </Suspense>
+                </Transition>
+            </template>
+        </RouterView>
+    </NConfigProvider>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
