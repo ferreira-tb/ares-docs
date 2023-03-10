f7ab5b7fc98ea50a8ab824e2e32b34a511fbc05b
ferreira-tb <109777783+ferreira-tb@users.noreply.github.com>
2023-03-10 00:04:34 -0300

    fix: altera base da URL

--- scripts/dist.mjs
+++ scripts/dist.mjs
@@ -8,13 +8,9 @@ const __dirname = path.dirname(__filename);
 try {
     const startTime = Date.now();
 
+    console.log('Movendo arquivos...');
+
     const distDir = path.resolve(__dirname, '../dist');
-
-    const indexFile = path.join(distDir, 'index.html');
-    let indexContent = await fs.readFile(indexFile, { encoding: 'utf-8' });
-    indexContent = indexContent.replace(/"\/assets\//g, '\"assets\/');
-    await fs.writeFile(indexFile, indexContent, { encoding: 'utf-8' });
-
     const docsDir = path.resolve(__dirname, '../../ares/docs');
     await fs.rm(docsDir, { recursive: true, force: true });
     await fs.rename(distDir, docsDir);
